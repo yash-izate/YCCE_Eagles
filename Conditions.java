02a0e9b4b8511089fa009c86f4b48dd7dea6c80b
Yash Izate <yashizate.softech@gmail.com>
2025-01-02 12:43:50 +0530

    update

--- Conditions.java
+++ Conditions.java
@@ -1,8 +1,16 @@
 public class Conditions {
   public static void main(String[] args) {
     
-    
+    int a = 10;
+    int b = 5;
+  
+    if(a>b) {
+      System.out.println("True");
+    }
 
+    // Type casting
+    double c = (double) a/b;
+    System.out.println(c);
 
   }
 }
