4494c301cf8b382f55b37210237c6b60608f22f8
Yash Izate <yashizate.softech@gmail.com>
2025-01-04 10:44:50 +0530

    fact recursion added

---
+++ Java_Program/Factorial.java
@@ -0,0 +1,17 @@
+package Java_Program;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int number = 5; // Example number
+        int result = factorial(number);
+        System.out.println("Factorial of " + number + " is: " + result);
+    }
+
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
