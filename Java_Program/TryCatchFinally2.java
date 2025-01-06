647627cba3c7fa9f1c1d48004e4e5494a6d78e2b
Yash Izate <132998594+yash-softech01@users.noreply.github.com>
2025-01-06 12:19:33 +0530

    try catch finally

---
+++ Java_Program/TryCatchFinally2.java
@@ -0,0 +1,16 @@
+public class TryCatchFinally2 {
+    @SuppressWarnings("UseSpecificCatch")
+    public static void getInteger() {
+        try {
+           throw new  ArithmeticException("Division by zero not allowed");
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+           System.out.println("Finally Block");
+        }
+    }
+
+    public static void main(String[] args) {
+        getInteger();
+    }
+}
