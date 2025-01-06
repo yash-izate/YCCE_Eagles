5a7913a199b857189d207c5d95960ed52359c5f4
Yash Izate <132998594+yash-softech01@users.noreply.github.com>
2025-01-06 12:31:33 +0530

    try catch exception example

---
+++ Java_Program/CustomException.java
@@ -0,0 +1,22 @@
+
+class CustomExceptionUser extends Exception {
+
+    public CustomExceptionUser(String message) {
+        super(message);
+    }
+
+}
+
+public class CustomException {
+
+    public static void main(String[] args) {
+
+        try {
+            throw new CustomExceptionUser("Exception");
+        } catch (CustomExceptionUser e) {
+            System.out.println("Caught: " + e.getMessage());
+        }
+
+    }
+
+}
