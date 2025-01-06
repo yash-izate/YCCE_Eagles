b94becc178b6c671a6e838dd519074f13a43949c
Yash Izate <132998594+yash-softech01@users.noreply.github.com>
2025-01-06 11:57:52 +0530

    try catch error handling

---
+++ Basic_DSA/TryCatch1.java
@@ -0,0 +1,17 @@
+
+public class TryCatch1 {
+
+    public static void getInteger() {
+        int a = Integer.parseInt("abc");
+        System.out.println("a = " + a);
+    }
+
+    public static void main(String[] args) {
+        try {
+            getInteger();
+        } catch (Exception e) {
+            System.out.println("Enter number only");
+        }
+    }
+
+}
